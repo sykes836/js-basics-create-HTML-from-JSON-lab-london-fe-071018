fcaaf0eea48b1211db19b774a1b9aca4af7b2f2d
sykes836 <bernard.sykes836@gmail.com>
2018-09-02 17:33:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
+function replaceContent (movie, part){
+    const item = document.getElementById(part);
+    while (item.firstChild) item.removeChild(item.firstChild);
+    const list = document.createElement ("ul");
+    movie[part].forEach (val=> {
+      const listItem = document.createElement ("li");
+      const itemAndValue = Object.keys(val).forEachKey=> 
+        (listItem.innerHTML+=${key}: {val[key]};
+      })
+      list.appendChild(listItem);
+    })
+    item.appendChild(list);
+}
+
+function replace(movie){
+  const mainTitle = document.getElementsByTagName("title")[0];
+  mainTitle.innerHTML = movie.title;
+  ["title", "director","genre","filmrating","description","audienceScore"].forEach(info=>{
+    const item = document.getElementById(info);
+    item.innerHTML = movie[info];
+  })
+  document.getElementById("poster").src-movie.poster;
+  replaceContent(movie, "cast");
+  replaceContent(movie, "reviews");
+}
+
+replace(movies.Titanic);
 document.addEventListener("DOMContentloaded", (e) => {
-  // invoking a function here will make sure it happens on page load
-  // your code here
+  Object.keys(movies).forEach(movie=>document.getElementById(movie).addEventListener("click", ()=>{
+  replace(movies[movie]);
+  }))
 });
